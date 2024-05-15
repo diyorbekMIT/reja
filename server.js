4f782773721d2aefd1efa5b66d4df882490754c2
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-05-15 21:00:09 +0900

    BRR: building express web server

--- server.js
+++ server.js
@@ -5,15 +5,7 @@ const app = express();
 const http = require('http');
 const fs = require('fs');
 
-let user;
-fs.readFile("./database/user.json", "utf-8",(err,data) => {
-    if(err) {
-        console.log("Error: ", err);
 
-    } else {
-        user = JSON.parse(data)
-    }
-})
 
 
 // 1: Kirish code
@@ -27,8 +19,14 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Running code
+app.post("/create-item", (req, res) => {
+    console.log(req);
+    res.json({test:"success"});
+});
+
+
 app.get("/", function(req, res) {
-    res.render("author", {user: user})
+  res.render("harid");
 });
 
 
