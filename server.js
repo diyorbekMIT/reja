ca853073d44b9372e5b57e77ddf8c0d5bf040883
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-05-18 17:00:37 +0900

    Resume finally

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const app = express();
 const http = require('http');
 const fs = require('fs');
 
-
+let user;
+fs.readFile("./database/user.json", "utf-8", (err,data) => {
+  if (err) {
+    console.log("Error:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 
 // 1: Kirish code
@@ -19,16 +26,9 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Running code
-app.post("/create-item", (req, res) => {
-    console.log(req);
-    res.json({test:"success"});
+app.get("/author", (req,res) => {
+  res.render("author", {user:user});
 });
-
-
-app.get("/", function(req, res) {
-  res.render("harid");
-});
-
 
 const server = http.createServer(app);
 let PORT = 3000;
