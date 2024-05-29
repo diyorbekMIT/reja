ea6811a6c6945e2593c388b7f5fc69251bed1ed1
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-05-30 07:27:56 +0900

    Final project

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongodb.connect(
       module.exports = client;
       const app = require("./app");
       const server = http.createServer(app);
-      let PORT = 3000;
+      let PORT = 3042;
       server.listen(PORT, () => {
         console.log(
           `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
