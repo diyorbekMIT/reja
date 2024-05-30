81c5bbca9a14003f5d1f49ee9296adfa6a89fea7
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-05-30 14:41:52 +0900

    Deploying to server and E task

--- train.js
+++ train.js
@@ -1,32 +1,14 @@
-// 2024-05-28
-// MIT 14 TASK D
+//E-TASK: 
 
-// Ikkita parametra ega function tuzing, va functioning
-// berilgan birinchi va ikkinchi parametr qiymatlari o'zaro to'liq
-// mos kelsa true qiymat qaytarsin
+//Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
+//MASALAN: getReverse("hello") return qilsin "olleh"
 
-// Masalan: checkContent("mitgroup", "gmtiprou");
-// Yuqoridagi misolda birinchi va ikkinchi parametr qiymatli bir xil
-// ya'ni bir xil harflar qatnashganligi uchun true qiymat qaytaradi.
-
-function checkContent(str1, str2) {
-   let count = 0;
-   for (let char of str1) {
-       if (str2.includes(char)) {
-           count ++;
-        }
-   
+function getReverse(string) {
+   reverse_text = [];
+   for (let char of string){
+      reverse_text.push(char)
    }
-   if (count == str1.length){
-      return true;
-   } 
-   else{
-      return false;
-   }
+   return reverse_text.reverse();
 }
 
-// Example usage:
-console.log(checkContent("mitgroup", "gmtiprou"));
-console.log(checkContent("mitgroup", "gmtipro")); 
-console.log(checkContent("abcdef", "ghijkl"))
-
+console.log(getReverse("hello"));
